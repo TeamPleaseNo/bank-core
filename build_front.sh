94851000b3671fd0fb4e7fd42f2212040dda4ecc
Dezzelshipc <foler126@gmail.com>
2023-02-03 23:42:08 +1000

    exit error

--- build_front.sh
+++ build_front.sh
@@ -7,8 +7,8 @@ retry() {
     do
         if (( attempt_num == max_attempts ))
         then
-            echo "Attempt $attempt_num failed and there are no more attempts left!"
-            return 1
+            echo "Attempt $attempt_num for building failed and there are no more attempts left!"
+            exit 1
         else
             echo "Attempt $attempt_num failed! Trying again in $attempt_num seconds..."
             sleep $(( attempt_num++ ))
